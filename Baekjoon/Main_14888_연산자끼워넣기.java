c4eb5fca9b55068d6e69d04c4518de221c7fa855
Kim Donghun <hun6211@gmail.com>
2019-08-22 10:43:09 +0900

    20190814

---
+++ Baekjoon/Main_14888_연산자끼워넣기.java
@@ -0,0 +1,73 @@
+package GitUpload;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main_14888_연산자끼워넣기 { // + - * /
+	
+	static int N, min = 1000000001, max = -1000000001;
+	static List<Integer> oper;
+	static List<Integer> A, visit; //생성하기(new)
+	
+	public static int calculate(List<Integer> list) {
+		int result = A.get(0);
+		for(int i = 0; i<N - 1; i++) {
+			switch(list.get(i)) {
+			case 0: //+				
+				result += A.get(i + 1); break;
+			case 1: //-
+				result -= A.get(i + 1); break;
+			case 2: //*
+				result *= A.get(i + 1); break;
+			case 3:
+				result /= A.get(i + 1); break;
+			}
+		}		
+		return result;
+	}
+	
+	public static void go(int cnt, List<Integer> list) {
+		if(cnt >= N - 1) {
+			int result = calculate(list);
+			if(result < min) min = result;
+			if(result > max) max = result;
+			return;
+		}
+		for(int i = 0; i<oper.size(); i++) {
+			if(visit.get(i) != 1) {
+				visit.set(i, 1);
+				list.add(oper.get(i)); //순열
+				go(cnt + 1, list);
+				list.remove(list.size() - 1); //리스트에 추가한 원소를 다시 제거해 줘야 한다.
+				visit.set(i,  0);
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		oper = new ArrayList<>();
+		visit = new ArrayList<>();
+		A = new ArrayList<>();
+		
+		for(int i = 0; i<N; i++) {
+			A.add(sc.nextInt());
+		}
+		for(int i = 0; i<4; i++) {
+			int a = sc.nextInt();
+			for(int j = 0; j<a; j++) {
+				oper.add(i); //+ - x /
+				visit.add(0);
+			}
+		}
+		List<Integer> lst = new ArrayList<Integer>();
+		go(0, lst);
+		
+		System.out.println(max);
+		System.out.println(min);
+	}
+
+}
---
+++ Baekjoon/Main_15686_치킨배달.java
@@ -0,0 +1,83 @@
+package GitUpload;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+class Pair {
+	int x, y;
+	
+	Pair(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+}
+public class Main_15686_치킨배달 {
+	public static int N, M, min = 100000001, visited[];
+	public static List<Pair> chickens = new ArrayList<Pair>();
+	public static List<Pair> home = new ArrayList<Pair>();
+	
+	public static void calDistance(List<Pair> chicken) {//치킨거리 계산하는 메서드
+		int sum = 0; //도시의 치킨거리
+		
+		for(int i = 0; i<home.size(); i++) {
+			int minDist = 100000000; //한 집에서의 치킨거리
+			for(int j = 0; j<chicken.size(); j++) {
+				int cX = chicken.get(j).x, cY = chicken.get(j).y;
+				int hX = home.get(i).x, hY = home.get(i).y; //j아님 i주의
+				int d = Math.abs(cX-hX) + Math.abs(cY-hY); //치킨거리
+				if(d<minDist) minDist = d;
+			}
+			sum += minDist;
+		}
+		if(sum < min)
+			min = sum;
+	}
+	
+	public static void go(int here, List<Pair> chicken) { //M개의 치킨집리스트 추출
+		if(chicken.size() >= M) {
+			//함수 호출
+			calDistance(chicken);
+			return;
+		}
+		
+		for(int i = here; i<chickens.size(); i++) {
+			if(visited[i] != 1) { //방문한적이 없으면
+				visited[i] = 1;
+				chicken.add(new Pair(chickens.get(i).x, chickens.get(i).y));
+				go(i + 1, chicken);
+				chicken.remove(chicken.size() - 1);
+				visited[i] = 0;
+			}
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		int a = 0;
+		for(int i = 0; i<N; i++) {
+			st = new StringTokenizer(bf.readLine());
+			for(int j = 0; j<N; j++) {
+				a = Integer.parseInt(st.nextToken());
+				if(a == 1)
+					home.add(new Pair(i, j));
+				else if(a == 2)
+					chickens.add(new Pair(i, j));				
+			}
+		}
+		visited = new int[chickens.size()];
+		
+		List<Pair> list = new ArrayList<Pair>();
+		go(0, list);
+		System.out.println(min);
+	}
+    
+}
