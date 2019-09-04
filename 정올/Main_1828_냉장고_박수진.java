a2b4e4caa3d63b4e6c9e508e1d349d3131e1dc18
hs <hsdeb11@gamil.com>
2019-09-04 09:24:49 +0900

    190829

---
+++ 정올/Main_1828_냉장고_박수진.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+ 
+public class Main_1828_냉장고_박수진 {
+    static class Pair {
+        int min, max;
+        public Pair(int min, int max) {
+            this.min = min;
+            this.max = max;
+        }
+    }
+    public static void main(String[] args) {
+         
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+         
+        int list[][] = new int[N][2];
+         
+        for(int i = 0; i<N; i++) {
+            list[i][0] = sc.nextInt();
+            list[i][1] = sc.nextInt();
+        }
+         
+        Arrays.sort(list, new Comparator<int[]>() {
+            public int compare(int[]a, int[]b) {
+                return a[0]- b[0];
+            }
+        });
+         
+        List<Pair> Ref = new ArrayList<Pair>();
+        int RefIdx = 0;
+        Ref.add(new Pair(list[0][0], list[0][1]));
+        for(int i = 1; i<list.length; i++) {
+            if(list[i][0] > Ref.get(RefIdx).max) {
+                RefIdx++;
+                Ref.add(new Pair(list[i][0], list[i][1]));
+                continue;
+            } else {
+                int RefMin = list[i][0], RefMax = Ref.get(RefIdx).max;
+                if(list[i][1] < RefMax) { //냉장고 범위 조절하기
+                    RefMax = list[i][1];
+                }
+                Ref.set(RefIdx, new Pair(RefMin, RefMax));
+            }
+        }
+         
+        System.out.println(RefIdx + 1);
+    }
+ 
+}
