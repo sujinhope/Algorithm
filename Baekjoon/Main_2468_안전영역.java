166110c9cd071d12e31d1ad87cce99e523f34ed9
sujin <hope772948@gamil.com>
2019-09-18 10:00:45 +0900

    190905

---
+++ Baekjoon/Main_2468_안전영역.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_2468_안전영역 {
+
+	static int N; //2<=N<=100
+	static int max = Integer.MIN_VALUE;
+	static int map[][];
+	static int dr[] = {-1, 1, 0, 0}, dc[] = {0, 0, -1, 1};
+	static boolean visited[][];
+	static boolean checked[] = new boolean[101]; //높이의 종류(?), 1<=높이<=100
+	
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		N = Integer.parseInt(bf.readLine());
+		map = new int[N][N];
+		
+		for(int i = 0; i<N; i++) {
+			st = new StringTokenizer(bf.readLine());
+			for(int j = 0; j<N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+				checked[map[i][j]] = true;
+			}
+		}		
+		
+		for(int i = 0; i<100; i++) {
+			if(checked[i]) { //map에 사용된 높이만 탐색
+				search(i);
+			}
+		}
+		if(max==0) max = 1;
+		System.out.println(max);
+	}
+
+	private static void search(int n) {
+		
+		visited = new boolean[N][N];
+		int safe = 0; //안전영역의 개수
+		
+		for(int i = 0; i<N; i++) {
+			for(int j = 0; j<N; j++){
+				if(map[i][j] > n && !visited[i][j]) {
+					visited[i][j] = true;
+					dfs(i, j, n);
+					safe++;
+				}
+			}
+		}
+		if(safe > max) max = safe;
+	}
+
+	private static void dfs(int r, int c, int n) {
+		
+		for(int i = 0; i<4; i++) {
+			int nr = r + dr[i];
+			int nc = c + dc[i];
+			if(nr>=0 && nr<N && nc>=0 && nc<N
+					&& map[nr][nc]>n && !visited[nr][nc]) {
+				//n보다 작거나 같은 높이만 탐색
+				visited[nr][nc] = true;
+				dfs(nr, nc, n);
+			}
+		}		
+		
+	}
+}
